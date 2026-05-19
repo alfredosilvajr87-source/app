501bc745c3e05131252d6fe021484a16440bbfed
Alfredo Junior <alfredo.silvajr87@gmail.com>
2026-05-19 23:44:43 +0100

    fix: rota /master fora do ProtectedRoute

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import ReportsPage from "./pages/ReportsPage";
 import SettingsPage from "./pages/SettingsPage";
 import UsersPage from "./pages/UsersPage";
 import DailyPrepPage from "./pages/DailyPrepPage";
+import MasterPage from "./pages/MasterPage";
 
 const ProtectedRoute = ({ children }) => {
   const { user, loading } = useAuth();
@@ -65,6 +66,7 @@ const AppRoutes = () => {
           </PublicRoute>
         }
       />
+      <Route path="/master" element={<MasterPage />} />
       <Route
         path="/"
         element={
